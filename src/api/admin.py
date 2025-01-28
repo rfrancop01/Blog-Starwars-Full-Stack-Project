b31de95137f5adc4a27f2a5afb9d06dcea13b13a
Ricardo Franco Pérez <rfrancop01@gmail.com>
2025-01-28 06:20:53 +0000

    Updated data

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+from .models import db, Users, Posts, Medias, Comments, Followers, CharacterFavorites, PlanetFavorites, Characters, Planets
 
 
 def setup_admin(app):
@@ -10,3 +10,13 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
+    admin.add_view(ModelView(Posts, db.session))  
+    admin.add_view(ModelView(Medias, db.session))
+    admin.add_view(ModelView(Comments, db.session))
+    admin.add_view(ModelView(Followers, db.session))
+    admin.add_view(ModelView(CharacterFavorites, db.session))
+    admin.add_view(ModelView(PlanetFavorites, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+
+
--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,76 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String, unique=False, nullable=False)
+    last_name = db.Column(db.String, unique=False, nullable=False)
+    phone = db.Column(db.String, unique=False, nullable=False)
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email} - {self.email}>'
 
     def serialize(self):
         # Do not serialize the password, its a security breach
         return {"id": self.id,
-                "email": self.email}
+                "email": self.email,
+                "password": self.password,
+                "is_active": self.is_active,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "phone": self.phone}
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=False, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+    date = db.Column(db.Date, unique=False, nullable=False)
+    image_url = db.Column(db.String, unique=True, nullable=False)
+
+
+class Medias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'), unique=False, nullable=False)
+    url = db.Column(db.String, unique=False, nullable=False)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class CharacterFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class PlanetFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    height = db.Column(db.String, unique=False, nullable=False)
+    mass = db.Column(db.String, unique=False, nullable=False)
+    hair_color = db.Column(db.Date, unique=False, nullable=False)
+    skin_color = db.Column(db.String, unique=False, nullable=False)
+    eye_color = db.Column(db.String, unique=False, nullable=False)
+    birth_year = db.Column(db.String, unique=False, nullable=False)
+    gender = db.Column(db.String, unique=False, nullable=False)
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    diameter = db.Column(db.String, unique=False, nullable=False)
+    rotation_period = db.Column(db.String, unique=False, nullable=False)
+    orbital_period = db.Column(db.Date, unique=False, nullable=False)
+    gravity = db.Column(db.String, unique=False, nullable=False)
+    population = db.Column(db.String, unique=False, nullable=False)
+    climate = db.Column(db.String, unique=False, nullable=False)
+    terrain = db.Column(db.String, unique=False, nullable=False)
